71608a0505565900bc23c2027e98084eeabb8755
philxws692 <63463378+philxws692@users.noreply.github.com>
2025-10-28 11:04:04 +0100

    feat: added cli args for better usability

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,33 @@
+use clap::Parser;
 use kt2j2t_rs::kt2j2t;
 
-fn main() {
-    let input_file_arg = std::env::args().nth(1).unwrap();
-    let input_file = std::fs::read_to_string(input_file_arg).unwrap();
+#[derive(Parser, Debug)]
+#[command(author, version, about, name = "kt2j2")]
+pub struct Args {
+    #[arg(short, long)]
+    /// File with JSON to transform
+    input: String,
 
-    let output_file_arg = std::env::args().nth(2).unwrap();
+    #[arg(short, long)]
+    /// File where transformed JSON should be outputted
+    output: String,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let input_file = std::fs::read_to_string(args.input).unwrap();
+
+    let output_file_arg = args.output;
 
     match kt2j2t(&input_file) {
         Ok(transformed_json) => {
             println!("JSON was successfully transformed");
-            std::fs::write(output_file_arg, serde_json::to_string_pretty(&transformed_json).unwrap()).unwrap();
+            std::fs::write(
+                output_file_arg,
+                serde_json::to_string_pretty(&transformed_json).unwrap(),
+            )
+            .unwrap();
         }
         Err(e) => {
             eprintln!("error with transforming json: {}", e);
